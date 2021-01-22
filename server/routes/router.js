dae95746492ded64dc386c1610dfee6f4012c80b
Michael Dunn <dunnmj42@gmail.com>
2021-01-22 17:37:58 -0600

    router object structure

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,12 +4,16 @@ const pool = require('../modules/pool');
 
 router.post('/', (req, res) => {
   let newFeedback = req.body
-  console.log(newFeedback);
+  
+  let feeling = newFeedback.feeling.feeling
+  let understanding = newFeedback.understanding.understanding
+  let support = newFeedback.support.support
+  let comments = newFeedback.comments.comments
 
   let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
                   VALUES ($1, $2, $3, $4);`;
 
-  pool.query(queryText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
+  pool.query(queryText, [feeling, understanding, support, comments])
     .then(result => {
       res.sendStatus(201);
     }).catch(error => {
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -7,11 +7,20 @@ function Review() {
   const feeling = useSelector((store) => store.feelingReducer);
   const understanding = useSelector((store) => store.understandingReducer);
   const support = useSelector((store) => store.supportedReducer);
-  const comment = useSelector((store) => store.commentReducer);
+  const comments = useSelector((store) => store.commentReducer);
 
   const history = useHistory();
 
   const nextPage = () => {
+
+    axios.post('/submit', {feeling, understanding, support, comments})
+    .then((response) => {
+      console.log(response)
+    }).catch((error) =>{
+      alert('error in submit')
+      console.error(error)
+    })
+
     history.push('/success')
   }
 
@@ -20,8 +29,8 @@ function Review() {
       <h1>Review Your Feedback</h1>
       <p>Feelings: {feeling.feeling}</p>
       <p>Understanding: {understanding.understanding}</p>
-      <p>Support: {support.supported}</p>
-      <p>Comments: {comment.comment}</p>
+      <p>Support: {support.support}</p>
+      <p>Comments: {comments.comments}</p>
       <button onClick={nextPage}>Submit</button>
     </div>
   )
