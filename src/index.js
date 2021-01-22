09903af0c9e0c5e1d158f28a0a9fe8d4c3842b5d
Michael Dunn <dunnmj42@gmail.com>
2021-01-22 14:54:04 -0600

    more install + reducers + provider + logger

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import "./index.css";
+import App from "./components/App/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feelingReducer = (state = 0, action) => {
+  return state;
+}
+
+const understandingReducer = (state = 0, action) => {
+  return state;
+}
+
+const supportedReducer = (state = 0, action) => {
+  return state;
+}
+
+const commentReducer = (state = '', action) => {
+  return state;
+}
+
+const reduxStore = createStore(
+  combineReducers({
+    feelingReducer,
+    understandingReducer,
+    supportedReducer,
+    commentReducer,
+  }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={reduxStore}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
