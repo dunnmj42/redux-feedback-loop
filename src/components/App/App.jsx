9ca38fcbcfc97ade7a40980c1349b6f29a98ce57
Michael Dunn <dunnmj42@gmail.com>
2021-01-22 15:35:44 -0600

    App router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import './App.css';
 import Header from '../Header/Header'
 
 function App() {
 
   return (
-    <div className='App'>
-      <Header />
-    </div>
-  );
+    <Router>
+      <div className='App'>
+        <Header />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
