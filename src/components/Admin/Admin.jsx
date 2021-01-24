ee5a0cd9e6d88304467757580270cb7841883360
Michael Dunn <dunnmj42@gmail.com>
2021-01-24 16:18:19 -0600

    break on admin

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -70,6 +70,7 @@ function Admin() {
           <h1>Feedback Results</h1>
         </CardContent>
       </Card>
+      <br/>
       <Card>
         <CardContent>
         <TableContainer component={Paper}>
